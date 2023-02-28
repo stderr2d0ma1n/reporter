fd033b349cf44f589b3e8ed1f9e0eb2aed5e7ebc
stderr2d0ma1n <113356347+stderr2d0ma1n@users.noreply.github.com>
2023-02-28 12:17:50 +0300

    add main file

---
+++ reporter.py
@@ -0,0 +1,37 @@
+from docxtpl import DocxTemplate
+from docxtpl import InlineImage
+import os
+import argparse
+import datetime
+
+
+parser = argparse.ArgumentParser(description='Simple tool for writing pentest report for multiply same vulnerabilies.')
+parser.add_argument(
+    '-t', '--template', type=str,
+    default=None, help='Provide template docx file'
+)
+parser.add_argument(
+    '-d', '--dir', type=str,
+    default='./', help='Provide directory with pocs'
+)
+parser.add_argument(
+    '-o', '--output', type=str,
+    default=datetime.datetime.now().strftime("%d-%m-%Y_%H:%M") + '.docx', help='Provide output file'
+)
+args = parser.parse_args()
+
+tpl = DocxTemplate('template.docx')
+content = {'hosts': []}
+for screen in os.listdir(args.dir):
+    if not screen.startswith('.'):
+        host_addr = screen.split('.')[0].replace('_', '.')
+        host_poc = InlineImage(tpl, image_descriptor=args.dir + '/' + screen)
+        content['hosts'].append({
+            'host_addr': host_addr,
+            'host_poc': host_poc
+        })
+    else:
+        continue
+
+tpl.render(content)
+tpl.save(args.output)
